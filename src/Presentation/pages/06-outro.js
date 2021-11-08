7d5edab11b9a475d59e074d96b2fd8b896093f98
Andrew Thian <andrew@stashaway.com>
2021-11-08 18:06:31 +0800

    add outro

---
+++ src/Presentation/pages/06-outro.js
@@ -0,0 +1,21 @@
+export const Outro = () => {
+  return (
+    <>
+      <section>
+        <h3>Outro</h3>
+      </section>
+      <section>
+        <p>Pros</p>
+      </section>
+      <section>
+        <p>cons</p>
+      </section>
+      <section>
+        <p>using in production</p>
+      </section>
+      <section>
+        <p>pitfalls</p>
+      </section>
+    </>
+  );
+};
